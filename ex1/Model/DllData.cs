1d611c2a8d7103686dda0c8119dbdc2fca23eba5
IdoAvTr <81075460+IdoAvTr@users.noreply.github.com>
2021-07-26 17:32:31 +0300

    fixed the call in MenuWindow to the constructor of MenuWindow

---
+++ ex1/Model/DllData.cs
@@ -0,0 +1,75 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1.Model
+{
+    public class DllData
+    {
+        private Dictionary<string, string> correlatives = new();
+        private Dictionary<string, List<DataPoint>> draws = new();
+        private Dictionary<string, List<float>> anomalies = new();
+
+        public Dictionary<string, string> Correlatives { get { return correlatives; } }
+        public Dictionary<string, List<DataPoint>> Draws { get { return draws; } }
+        public Dictionary<string, List<float>> Anomalies { get { return anomalies; } }
+
+        public DllData(string pathToDll, string pathFileNormalFile, string pathFileExceptionFile, List<string> names)
+        {
+            Type[] externDllTypes = Assembly.LoadFile(@pathToDll).GetTypes();
+            dynamic ad = Activator.CreateInstance(externDllTypes[0], pathFileNormalFile, pathFileExceptionFile, names.ToArray<string>());
+
+            //corellations
+            int n = ad.getCoupleNum();
+            for (int i = 0; i < n; i++)
+            {
+                if(!correlatives.ContainsKey(ad.getCorrelatives(i, true))){
+
+                    correlatives.Add(ad.getCorrelatives(i, true), ad.getCorrelatives(i, false));
+                }
+                if(!correlatives.ContainsKey(ad.getCorrelatives(i, true))){
+
+                    correlatives.Add(ad.getCorrelatives(i, false), ad.getCorrelatives(i, true));
+                }
+            }
+            foreach (string s in names)
+            {
+                if (!correlatives.ContainsKey(s))
+                {
+                    correlatives.Add(s, " ");
+                }
+            }
+
+            //Draw
+            n = ad.getDrawNUmber();
+            foreach (string attr in names)
+            {
+                List<DataPoint> points = new();
+                for (int i = 0; i < n; i++)
+                {
+                    DataPoint tmp = new DataPoint(ad.getDraw(attr, i, true), ad.getDraw(attr, i, false));
+                    points.Add(tmp);
+                }
+                draws.Add(attr, points);
+            }
+
+
+            //Anomalies
+            n = names.Count;
+            for (int i = 0; i < n; i++)
+            {
+                int m = ad.getNumOfAnomalies(names[i]);
+                anomalies.Add(names[i], new List<float>());
+                for (int j = 0; j < m; j++)
+                {
+                    anomalies[names[i]].Add(ad.getAnomaly(names[i], j));
+                }
+            }
+
+        }
+    }
+}
---
+++ ex1/Model/MainGraph.cs
@@ -0,0 +1,72 @@
+﻿using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Regression;
+
+namespace ex1.Model
+{
+    public class MainGraph : ObservableCollection<ScatterPoint>
+    {
+        private IData data;
+        private string attrName;
+        DllData dllData;
+        public Dictionary<string, List<float>> Anomalies { get { return dllData.Anomalies; } }
+        public string AttrName
+        {
+            get
+            {
+                return attrName;
+            }
+            set
+            {
+                if (attrName != value)
+                {
+                    attrName = value;
+                }
+            }
+        }
+
+        private string correlativeName;
+        public string CorrelativeName
+        {
+            get
+            {
+                return correlativeName;
+            }
+            set
+            {
+                if (correlativeName != value)
+                {
+                    correlativeName = value;
+                    ClearItems();
+                }
+            }
+        }
+
+        public void setData(IData info, DllData dll)
+        {
+            data = info;
+            dllData = dll;
+        }
+
+
+        public void update(int frameIndex)
+        {
+            ClearItems();
+            int i;
+            for (i = Math.Max(0, frameIndex - 300); i <= frameIndex; i++)
+            {
+                if (!Anomalies[attrName].Contains(i))
+                {
+                    Add(new ScatterPoint(data.getElement(AttrName, i), data.getElement(correlativeName, i), 1, 200));
+                    continue;
+                }
+                Add(new ScatterPoint(data.getElement(AttrName, i), data.getElement(correlativeName, i), 1, 800));
+            }
+        }
+    }
+}
